0d14c022c6a113cd5cf97a32e00546509753749d
DeveloperSaikat <saikatk594@gmail.com>
2023-07-09 16:57:43 +0530

    signup and signin logic added

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,53 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequetError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
-router.post('/api/users/signin', () => {
+router.post('/api/users/signin',
+    [
+        body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+        body('password')
+        .trim()
+        .notEmpty()
+        .withMessage('You must supply a password')
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => { 
+        const { email, password } = req.body;
 
-});
+        const existingUser = await User.findOne({email})
+        if(!existingUser) {
+            console.log('User does not exist');
+            throw new BadRequetError('Invalid Credentials');
+        }
+
+        const passwordsMatch = await Password.compare(existingUser.password, password);
+
+        if(!passwordsMatch) {
+            console.log('Password dont match');
+            throw new BadRequetError('Invalid Credentials');
+        }
+
+        const userJWT = jwt.sign({
+            id: existingUser.id,
+            email: existingUser.email
+        }, 
+        'somesupersecret');
+
+        req.session = {
+            jwt: userJWT
+        }
+
+        res.status(200).send(existingUser);
+    }
+);
 
 export { router as signinRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,28 +1,29 @@
 import express, {Request, Response} from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError  } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { body } from 'express-validator';
+
+import { User } from '../models/user';
+import { BadRequetError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.get('/api/users/signup',[
+router.post('/api/users/signup',[
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({min: 4, max: 20}).withMessage('Password must be between 4 to 20 characters')
-], (req: Request, res: Response) => {
-    const errors = validationResult(req); // The validators that were added will check for errors and if received then will add here
+], 
+validateRequest,
+async (req: Request, res: Response) => {
+    const { email, password } = req.body; 
+    const existingUser = await User.findOne({ email });
 
-    if(!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
+    if (existingUser) {
+        throw new BadRequetError('Email in use');
     }
 
-    console.log('Creating a user');
-    throw new DatabaseConnectionError();
+    const user = User.build({email, password});
+    await user.save();
 
-    // const { email, password } = req.body; 
-
-    // res.send({msg: "Done"})
-
-        
+    res.status(201).send(user);
 });
 
 export { router as signupRouter };
